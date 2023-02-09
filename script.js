b185cd7afefbac2f119d525e9264aab6021df940
woojin <tbvc@naver.com>
2023-02-10 00:11:44 +0900

    단어 업데이트

--- script.js
+++ script.js
@@ -89,10 +89,10 @@ function checkGuess () {
         return
     }
 
-    // if (!WORDS.includes(guessString)) {
-    //     toastr.error("Word not in list!")
-    //     return
-    // }
+    if (!WORDS.includes(guessString)) {
+        toastr.error("단어가 없습니다")
+        return
+    }
 
     
     for (let i = 0; i < 5; i++) {
